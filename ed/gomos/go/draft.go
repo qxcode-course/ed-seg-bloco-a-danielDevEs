fc1c71baa23268ed2a4d6ecd7a0a1d2bd2c105a0
Daniel <danielandrade07@alu.ufc.br>
2026-03-15 15:12:39 -0300

    Alteração missão gomos - como se repetia em todos os cases deixei fora -

--- ed/gomos/go/draft.go
+++ ed/gomos/go/draft.go
@@ -10,50 +10,31 @@ func main() {
     // for i := range cobra{
     //     cobra[i] = make([]int, qtdgomos)
     // }
+    
     lineX, lineY := make([]int, qtdgomos), make([]int, qtdgomos)
     // var posX, posY int
 
     for i := range qtdgomos{
         fmt.Scan(&lineX[i], &lineY[i])
-        // lineX = append(lineX, posX)
-        // lineY = append(lineY, posY)
     }
     // fmt.Println(lineX)
     // fmt.Println(lineY)
 
+    for i := range len(lineX) - 1{
+        lineX[qtdgomos - i -1] = lineX[qtdgomos - i - 2]
+    }
+    for i := range len(lineY) - 1{
+                lineY[qtdgomos - i - 1] = lineY[qtdgomos - i - 2]
+    }
+
     switch direction {
         case "R":
-             for i := range len(lineX) - 1{
-                lineX[qtdgomos - i - 1] = lineX[qtdgomos - i - 2]
-            }
-            for i := range len(lineY) - 1{
-                lineY[qtdgomos - i - 1] = lineY[qtdgomos - i - 2]
-            }
             lineX[0] += 1
         case "L":
-             for i := range len(lineX) - 1{
-                lineX[qtdgomos - i - 1] = lineX[qtdgomos - i - 2]
-            }
-            for i := range len(lineY) - 1{
-                lineY[qtdgomos - i - 1] = lineY[qtdgomos - i - 2]
-            }
             lineX[0] -= 1
         case "U":
-            // for i := 0; i < len(lineX) - 1; i++
-            for i := range len(lineX) - 1{
-                lineX[qtdgomos - i - 1] = lineX[qtdgomos - i - 2]
-            }
-            for i := range len(lineY) - 1{
-                lineY[qtdgomos - i - 1] = lineY[qtdgomos - i - 2]
-            }
             lineY[0] -= 1
         case "D":
-            for i := range len(lineX) - 1{
-                lineX[qtdgomos - i -1] = lineX[qtdgomos - i - 2]
-            }
-            for i := range len(lineY) - 1{
-                lineY[qtdgomos - i - 1] = lineY[qtdgomos - i - 2]
-            }
             lineY[0] += 1
     }
 
